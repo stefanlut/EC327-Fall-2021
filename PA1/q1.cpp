e79e79f6ddfb3f917d85ae514a2117950e48f579
stefanlut <slutschg@bu.edu>
2021-09-23 17:20:39 -0400

    Started PA1 Q1

--- PA1/q1.cpp
+++ PA1/q1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include <math.h>
 
 using namespace std;
@@ -6,10 +7,19 @@ using namespace std;
 int main()
 {
     char inputCoordinates[32];
+    int ch,a;
     cout << "Enter three x,y coordinates:"<< endl;
-    cin >> inputCoordinates;
-
+    // cin >> noskipws >> inputCoordinates;
+    a = 0;
     
-
+    
+    while((ch=getchar()) != '\n')
+    {
+        inputCoordinates[a] = ch;
+        a++;
+        if(a == 31) break;
+    }
+    inputCoordinates[a] = '\0';
+    cout<< "The coordinates are: " << inputCoordinates << endl;
     return 0;
 }
