56b78615337c89154de1bc0bc7edfd43733a1480
Stefan Lutschg <hirzli2@gmail.com>
2021-09-29 16:03:54 -0400

    Lab 3

--- Labs/is_divisible_by.cpp
+++ Labs/is_divisible_by.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int main()
 {
-    int num;
+    int num, counter = 1;
     cout << "What number would you like me to check? ";
     cin >> num;
     if(num < 0)
@@ -17,8 +17,20 @@ int main()
         cout << "1 is not a prime number" << endl;
         return 0;
     }
-
-
+    cout << "1 ";
+    for(int i = 2; i <=num;i++)
+    {
+        if (num % i == 0)
+        {
+            cout << i << " ";
+            counter++;
+        }
+    }
+    cout << endl;
+    if(counter > 1)
+        cout << "This number is not Prime!" << endl;
+    else
+        cout << "This number is Prime!" << endl;
 
     return 0;
 }
