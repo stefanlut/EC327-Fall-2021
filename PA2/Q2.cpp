9c59a2526ec68035220e08139388b0a208fc9738
stefanlut <slutschg@bu.edu>
2021-10-18 21:38:40 -0400

    kill me now

--- PA2/Q2.cpp
+++ PA2/Q2.cpp
@@ -2,37 +2,41 @@
 #include "FileFunctions.h"
 #include "Statistics.h"
 using namespace std;
-int * DoubleAndReverse(int *list, int size)
+int * DoubleAndReverse(int *list,int size)
 {
-    int newArray[size * 2];
+    int *res;
+    
     for(int i = 0; i < size; i++)
     {
-        newArray[i] = *(list + i);
+        res[i] = *(list + i);
         
     }
-    for(int i = size; i > 0; i--)
+    for(int i = 0; i < size ; i++)
     {
-        newArray[size * 2 - i] = *(list + i);
+        res[i+size] =*(list + i);
     }
-    return newArray;
+    return res;
 }
 int main()
 {
     char filename[10] = {'d','a','t','a','.','t','x','t','\0'};
     int size;
     int myArray[32];
-    
+    int *newArray;
     cout << "Writing file: " << filename << endl;
     WriteRandomData(10, 1000,filename);
 
     cout << "Reading file: " << filename << endl;
     ReadData(filename,size,myArray);
     cout << "Array size is " << size << endl;
-    //int newArray[size*2] = DoubleAndReverse(myArray, size);
+    
     cout << "Double and reversed is [";
-    // for(int i = 0; i < size * 2; i++)
-    // {
-    //     cout << " " << 
-    // }
+    newArray = DoubleAndReverse(myArray, size);
+    
+    for(int i = 0; i < size * 2; i++)
+    {
+        cout << " " << *(newArray + i);
+    }
+    cout << ']' << endl;
     return 0;
 }
