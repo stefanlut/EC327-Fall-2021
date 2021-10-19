c2d9c535614ac0467d8eb1015d5211a155af2557
stefanlut <slutschg@bu.edu>
2021-10-18 21:42:03 -0400

    ahhh

--- PA2/Q2.cpp
+++ PA2/Q2.cpp
@@ -13,7 +13,7 @@ int * DoubleAndReverse(int *list,int size)
     }
     for(int i = 0; i < size ; i++)
     {
-        res[i+size] =*(list + i);
+        res[i+size] =*(list + (size - i));
     }
     return res;
 }
